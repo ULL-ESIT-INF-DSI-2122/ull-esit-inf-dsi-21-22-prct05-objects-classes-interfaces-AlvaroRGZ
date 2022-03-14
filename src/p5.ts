3bd7ebadfa29837e655bc514b110b48bcad211d6
AlvaroRGZ <alu0101362953@ull.edu.es>
2022-03-14 10:46:20 +0000

    Modificacion

--- src/p5.ts
+++ src/p5.ts
@@ -1,3 +1,99 @@
-export function a(): number {
-  return 1;
+/**
+* Representa la informacion de una persona
+*/
+
+export class Persona {
+  constructor(private nombre: string, private apellidos: string,
+              private fecha: string, private genero: string){
+  }
+  getNombre(): string {
+    return this.nombre;
+  }
+  getApellidos(): string {
+    return this.apellidos;
+  }
+  getFecha(): string {
+    return this.fecha;
+  }
+  getGenero(): string {
+    return this.genero;
+  }
+
+  setNombre(g: string) {
+    this.nombre = g;
+  }
+  setApellidos(g: string) {
+    this.apellidos = g;
+  }
+  setFecha(g: string) {
+    this.fecha = g;
+  }
+  setGenero(g: string) {
+    this.genero = g;
+  }
+}
+
+/**
+* Representa la informacion de una persona estudiante
+* @extends Persona
+*/
+export class Estudiante extends Persona {
+  private correo: string;
+  constructor(nombre: string, apellidos: string,
+              fecha: string, genero: string, mail: string) {
+      super(nombre, apellidos, fecha, genero);
+      this.correo = mail;
+    }
+  getCorreo(): string {
+    return this.correo;
+  }
+  setCorreo(c: string){
+    this.correo = c;
+  }
+}
+
+/**
+* Representa la informacion de una persona profesora
+* @extends Persona
+*/
+export class Profesor extends Persona {
+  private numero: number;
+  constructor(nombre: string, apellidos: string,
+              fecha: string, genero: string, num: number) {
+      super(nombre, apellidos, fecha, genero);
+      this.numero = num;
+    }
+  getNumero(): number {
+    return this.numero;
+  }
+  setNumero(c: number){
+    this.numero = c;
+  }
+}
+
+/**
+ * Representa el cunjunto de profesores que imparten una
+ * asignatura con @nombre y el conjunto de profesores que
+ * la imparten @profesores : profesor[] y los estudiantes
+ * que la cursan @estudiantes : estudiante[]
+ */
+export class Asignatura {
+  constructor(private nombre: string, private profesores: Profesor[],
+              private alumnos: Estudiante[]) {}
+
+  getNombre(): string {
+    return this.nombre;
+  }
+  listaProfesores(): Profesor[] {
+    this.profesores.forEach((element) => {
+      console.log(element.getNombre() + ' ' + element.getNumero().toString());
+    });
+    return this.profesores;
+  }
+  listaAlumnos(): Estudiante[] {
+    this.alumnos.forEach((element) => {
+      console.log(element.getNombre() + ' ' + element.getCorreo());
+    });
+    return this.alumnos;
+  }
 }
--- tests/p5.spec.ts
+++ tests/p5.spec.ts
@@ -1,9 +1,96 @@
 import 'mocha';
 import {expect} from 'chai';
-import {a} from '../src/p5';
+import {Persona, Estudiante, Profesor, Asignatura} from '../src/p5';
 
-describe('a', () => {
-  it('a', () => {
-    expect(a()).to.be.eql(1);
+describe('Pruebas modificacion', () => {
+  describe('Una persona tiene', () => {
+    const p1 = new Persona('alv', 'Rod Gom', '1', 'M');
+    it('Nombre', () => {
+      expect(p1.getNombre()).to.be.eql('alv');
+    });
+    it('Apellidos', () => {
+      expect(p1.getApellidos()).to.be.eql('Rod Gom');
+    });
+    it('fecha de Nacimiento', () => {
+      expect(p1.getFecha()).to.be.eql('1');
+    });
+    it('Genero', () => {
+      expect(p1.getGenero()).to.be.eql('M');
+    });
+
+    it('Se pueden cambiar', () => {
+      p1.setNombre('x');
+      p1.setApellidos('p');
+      p1.setFecha('2');
+      p1.setGenero('F');
+      expect(p1.getNombre()).to.be.eql('x');
+      expect(p1.getApellidos()).to.be.eql('p');
+      expect(p1.getFecha()).to.be.eql('2');
+      expect(p1.getGenero()).to.be.eql('F');
+    });
+  });
+
+  describe('Una estudiante tiene', () => {
+    const es = new Estudiante('alv', 'Rod Gom', '1', 'M', 'ull.es');
+    it('Nombre', () => {
+      expect(es.getNombre()).to.be.eql('alv');
+    });
+    it('Apellidos', () => {
+      expect(es.getApellidos()).to.be.eql('Rod Gom');
+    });
+    it('fecha de Nacimiento', () => {
+      expect(es.getFecha()).to.be.eql('1');
+    });
+    it('Genero', () => {
+      expect(es.getGenero()).to.be.eql('M');
+    });
+    it('Correo', () => {
+      expect(es.getCorreo()).to.be.eql('ull.es');
+    });
+    it('Podemos cambiar correo', () => {
+      es.setCorreo('ull.com');
+      expect(es.getCorreo()).to.be.eql('ull.com');
+    });
+  });
+  describe('Un profesor tiene', () => {
+    const pr = new Profesor('alv', 'Rod Gom', '1', 'M', 1234);
+    it('Nombre', () => {
+      expect(pr.getNombre()).to.be.eql('alv');
+    });
+    it('Apellidos', () => {
+      expect(pr.getApellidos()).to.be.eql('Rod Gom');
+    });
+    it('fecha de Nacimiento', () => {
+      expect(pr.getFecha()).to.be.eql('1');
+    });
+    it('Genero', () => {
+      expect(pr.getGenero()).to.be.eql('M');
+    });
+    it('Numero', () => {
+      expect(pr.getNumero()).to.be.eql(1234);
+    });
+    it('Se puede cambiar el numero', () => {
+      pr.setNumero(6789);
+      expect(pr.getNumero()).to.be.eql(6789);
+    });
+  });
+
+  describe('Una asignatura:', () => {
+    const pr1 = new Profesor('pedro', 'abc', '1', 'M', 1234);
+    const pr2 = new Profesor('javi', 'def', '4', 'M', 6789);
+
+    const e1 = new Estudiante('alvaro', 'Rod Gom', '1', 'M', 'ull.es');
+    const e2 = new Estudiante('carlos', 'gs dsg', '1', 'M', 'ulpgc.es');
+    const a = new Asignatura('dsi', [pr1, pr2], [e1, e2]);
+
+    it('Nombre', () => {
+      expect(a.getNombre()).to.be.eql('dsi');
+    });
+    it('Se listan y devuelven los profesores', () => {
+      expect(a.listaProfesores()).to.be.deep.equal([pr1, pr2]);
+    });
+    it('Se listan y devuelven los alumnos', () => {
+      expect(a.listaAlumnos()).to.be.deep.equal([e1, e2]);
+    });
   });
 });
